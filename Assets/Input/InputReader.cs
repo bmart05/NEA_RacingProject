67a7a859bf706a00cc64a1acc03536c3f42d0a4e
Ben Martin <57049373+bmart05@users.noreply.github.com>
2024-08-03 13:01:31 +0100

    Setup input system and created input events

---
+++ Assets/Input/InputReader.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using static PlayerControls;
+
+namespace Input
+{
+    [CreateAssetMenu(menuName = "InputReader", fileName = "InputReader", order = 0)]
+    public class InputReader : ScriptableObject, IPlayerActions
+    {
+        private PlayerControls controls;
+
+        public Action<float> TurnEvent;
+        public Action<bool> AccelerateEvent;
+        public Action<bool> FireEvent;
+        
+        private void OnEnable()
+        {
+            if (controls == null)
+            {
+                controls = new PlayerControls();
+                controls.Player.SetCallbacks(this);
+            }
+
+            controls.Player.Enable(); // split this into a separate script to stop players from moving at the end/start of a race
+        }
+        
+        public void OnTurn(InputAction.CallbackContext context)
+        {
+            TurnEvent?.Invoke(context.ReadValue<float>());
+        }
+
+        public void OnAccelerate(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                AccelerateEvent?.Invoke(true);
+            }
+            else if (context.canceled)
+            {
+                AccelerateEvent?.Invoke(false);
+            }
+        }
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                FireEvent?.Invoke(true);
+            }
+            else if (context.canceled)
+            {
+                FireEvent?.Invoke(false);
+            }
+        }
+    }
+}
